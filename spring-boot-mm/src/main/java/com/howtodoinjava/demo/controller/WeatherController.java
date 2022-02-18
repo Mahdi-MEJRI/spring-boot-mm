ea745910637b8a2797921ed5a5dee32257a67bc6
Mahdi-MEJRI <mahdi.mejri7817@gmail.com>
2022-02-18 16:02:54 +0100

    part 3

--- spring-boot-mm/src/main/java/com/howtodoinjava/demo/controller/WeatherController.java
+++ spring-boot-mm/src/main/java/com/howtodoinjava/demo/controller/WeatherController.java
@@ -5,12 +5,17 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import com.howtodoinjava.demo.model.Weather;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.howtodoinjava.demo.model.Weather;
 
+@Api(value="WeatherController", description = "REST Apis pour la meteo !")
 @RestController
 public class WeatherController {
 
@@ -24,22 +29,31 @@ public class WeatherController {
         weatherList.add(new Weather("Madrid","04000","es","19"));
     }
 
+    @ApiOperation(value = "Get list of weather in towns", response = Iterable.class, tags = "getWeather")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Suceess|OK"),
+            @ApiResponse(code = 401, message = "not authorized!"),
+            @ApiResponse(code = 403, message = "forbidden!!!"),
+            @ApiResponse(code = 404, message = "not found!!!") })
     @RequestMapping("/weather")
     public List<Weather> getWeather()
     {
         return weatherList;
     }
 
+    @ApiOperation(value = "Get weather in specefic tow ", response = Weather.class, tags = "getWeatherInTown")
     @RequestMapping(value = "/weather/{ville}")
     public Weather getWeather(@PathVariable(value = "ville") String ville) {
         return weatherList.stream().filter(x -> x.getVille().equalsIgnoreCase(ville)).collect(Collectors.toList()).get(0);
     }
 
+    @ApiOperation(value = "Get specific Student in the System ", response = Weather.class, tags = "getWeatherByZipCode")
     @RequestMapping(value = "/weatherByZipCode/{zipCode}")
     public Weather getWeatherByZipCode(@PathVariable(value = "zipCode") String zipCode) {
         return weatherList.stream().filter(x -> x.getZipCode().equalsIgnoreCase(zipCode)).collect(Collectors.toList()).get(0);
     }
 
+    @ApiOperation(value = "Get list of weather by country", response = Iterable.class, tags = "getWeatherBycountry")
     @RequestMapping(value = "/weatherByPays/{pays}")
     public List<Weather> getWeatherByPays(@PathVariable(value = "pays") String pays) {
         return weatherList.stream().filter(x -> x.getPays().equalsIgnoreCase(pays)).collect(Collectors.toList());
--- spring-boot-mm/src/main/java/com/howtodoinjava/demo/model/Weather.java
+++ spring-boot-mm/src/main/java/com/howtodoinjava/demo/model/Weather.java
@@ -1,11 +1,18 @@
 package com.howtodoinjava.demo.model;
 
+import io.swagger.annotations.ApiModelProperty;
+
 public class Weather {
 
+    @ApiModelProperty(notes = "Nom ville",name="ville",required=true,value="test ville")
     private String ville;
+    @ApiModelProperty(notes = "Code postal",name="zipCode",required=true,value="test zipCode")
     private String zipCode;
+    @ApiModelProperty(notes = "Nom pays",name="pays",required=true,value="test pays")
     private String pays;
+    @ApiModelProperty(notes = "Temperature dans la ville",name="meteo",required=true,value="test meteo")
     private String meteo;
+
 
     public Weather(String ville, String zipCode, String pays, String meteo) {
         this.ville = ville;
